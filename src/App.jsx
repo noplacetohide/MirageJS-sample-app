5fa4dfdf44d7d765e9cbfc8897da677e6310ce52
noplacetohide <nikhildwivedi007@gmail.com>
2025-06-24 11:15:11 +0530

    fix for app.js

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,11 @@ export default function App() {
 
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold">User List</h1>
-      <ul className="mt-4">
+      <h1 className="text-2xl font-bold flex justify-start items-center p-4 bg-gray-100 rounded-sm text-gray-500 shadow shadow-gray-400">User List</h1>
+      {!!(users?.length < 1) && <h3 className="p-4 my-2 rounded-sm bg-red-100 shadow shadow-red-300 text-md font-semibold text-gray-600">No Data Found</h3>}
+      <ul>
         {users.map((user) => (
-          <li key={user.id} className="mb-2">
+          <li key={user.id} className="p-4 my-2 rounded-sm bg-green-100 shadow shadow-green-600 text-lg font-semibold text-gray-600">
             {user.firstName} {user.lastName} - {user.email}
           </li>
         ))}
